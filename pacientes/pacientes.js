7180539b45a50816b8de2aaedcd454dc0dbbc9a2
snowcat-web <63032695+snowcat-web@users.noreply.github.com>
2020-04-04 14:43:51 -0600

    Agregando pacientes nuevos

---
+++ pacientes/pacientes.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+    'use strict';
+    
+      
+    function test() {
+        
+        //alert("This alert box was called with the onload event");
+    }
+    
+
+    function init() {
+        test();        
+    }
+
+    init();
+
+})(jQuery)
---
+++ pacientes/pacientescontroller.js
@@ -0,0 +1,5 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {  
+  $scope.firstName= "Paty";
+  $scope.lastName= "Reyes";
+});
